ea63e520368161a369abf5484f0b183f71223646
Your Name <you@example.com>
2023-04-06 16:39:38 +0100

    Login successful

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-import axios from 'axios';
-const baseUrl = 'http://localhost/api/login.php';
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+import axios from "axios";
+const baseUrl = "http://192.168.0.60/api/login.php";
+// const baseUrl = "http://localhost/api/login.php";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
+      console.log("email:", email);
+      console.log("password:", password);
+  
       const response = await axios.post(baseUrl, {
-        user_name: email,
-        user_pass: password,
+        "user_name": email,
+        "user_pass": password,
       });
       // Handle successful login
       console.log(response.data);
     } catch (error) {
       // Handle login error
       console.error(error);
+      console.log(error.response.data);
       throw error;
     }
   };
-
+  
 
   return (
     <View style={styles.container}>
@@ -46,15 +51,15 @@ const LoginForm = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   input: {
-    width: '100%',
+    width: "100%",
     marginBottom: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     padding: 10,
   },
 });
